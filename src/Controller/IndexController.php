cf2fdcee52674bd61384d296f77bc3ea8e69832c
Gabriel PERINO <perinogabriel1@gmail.com>
2022-01-28 00:22:23 +0100

    [index] : simplify code ==> change block code to for block

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,8 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Entity\Avis;
+use App\Entity\Produit;
+use App\Form\ProduitType;
+use App\Repository\ProduitRepository;
+//use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
@@ -12,29 +19,31 @@ class IndexController extends AbstractController
      * @Route("/index", name="index")
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(ProduitRepository $produitRepository): Response
     {
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
+            'produits' => $produitRepository->findAll(),
         ]);
     }
         /**
-     * @Route("/BMX", name="BMX")
+     * @Route("/BMX", name="BMX", methods={"GET"})
      */
-    public function BMX(): Response
+    public function BMX(ProduitRepository $produitRepository): Response
     {
         return $this->render('index/bmx.html.twig', [
             'controller_name' => 'IndexController',
+            'produits' => $produitRepository->findAll(),
         ]);
     }
     /**
      * @Route("/panier", name="Panier")
      */
-    public function panier(): Response
+    public function panier(/*Produit $produit*/): Response
     {
-
         return $this->render('index/panier.html.twig', [
             'controller_name' => 'bienvenue',
+//            'produit' => $produit,
         ]);
     }
 }
